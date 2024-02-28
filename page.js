d8378b0674600d4f7a6f917fa342de0effaffbb0
changaimaraSA <35507028+changaimaraSA@users.noreply.github.com>
2024-02-28 13:34:54 -0800

    removed js from html, added event handlers
    index.html - removed the js from clickme button
    page.js - added page.init for initialzing elements, added load event handler for the window

--- page.js
+++ page.js
@@ -5,5 +5,14 @@ const page = {
         const para = document.createElement("p");
         para.innerHTML = "Hello, " + fUsername.value +"!";
         outputDiv.appendChild(para);
+    },
+
+    init: function(){
+        console.log("Page loaded!");
+        const fClickMeButton = document.querySelector("#clickMeButton");
+        fClickMeButton.addEventListener("click", page.default);
+        console.log("Done initializing!")
     }
 }
+
+window.addEventListener("load", page.init);
